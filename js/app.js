6e53c1726a4c347e40b22d47f00cadca368fe7b1
RamiPizasc <ramiropiza97@gmail.com>
2021-12-03 18:38:46 -0300

    modificamos hoja de estilos, index y ajustamos js

--- js/app.js
+++ js/app.js
@@ -2,106 +2,108 @@ let container = document.getElementById("seccion");
 
 const comprarTicket = () => {
   container.innerHTML = `
-  <div class="container">
-  <div class="row">
-      <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 py-3">
-          <div class="card-group gap-1">
-              <div class="card">
-                  <div class="card-body border border-primary mr-1">
-                      <h5 class="card-title text-center">Estudiante</h5>
-                      <p class="card-text text-center">Tienen un descuento</p>
-                      <p class="card-title text-center fw-bolder">80%</p>
-                      <p class="card-text text-center">
-                          <small class="text-muted">
-                          * presentar documentación
-                          </small>
-                      </p>
-                  </div>
-              </div>
-              <div class="card">
-                  <div class="card-body border border-info mr-1 ">
-                      <h5 class="card-title text-center">Trainee</h5>
-                      <p class="card-text text-center">Tienen un descuento</p>
-                      <p class="card-title text-center fw-bolder">50%</p>
-                      <p class="card-text text-center">
-                          <small class="text-muted">
-                          * presentar documentación
-                          </small>
-                      </p>
-                  </div>
-              </div>
-              <div class="card">
-                  <div class="card-body border border-warning mr-1">
-                      <h5 class="card-title text-center">Junior</h5>
-                      <p class="card-text text-center">Tienen un descuento</p>
-                      <p class="card-title text-center fw-bolder">15%</p>
-                      <p class="card-text text-center">
-                          <small class="text-muted">
-                          * presentar documentación
-                          </small>
-                      </p>
-                  </div>
-              </div>
-          </div>
-      </div> 
-  </div>
-  <div class="row">
-      <div class="col text-uppercase text-center">
-          <small>venta</small>
-          <h2>valor de ticket $200</h2>
-      </div>
-  </div>
-  <div class="row">
-      <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 py-1">
-          <form action="index.html" method="POST">
-              <div class="form-row">
-                  <div class="d-flex py-2">
-                      <div class="form-group col-6 col-md-6 p-1">
-                          <input type="text" class="form-control" placeholder="Nombre" />
-                      </div>
-                      <div class="form-group col-6 col-md-6 p-1">
-                          <input type="text" class="form-control" placeholder="Apellido" />
-                      </div>
-                  </div>
-                      <div class="form-group col-12 col-md-12 p-1">
-                          <input type="email" class="form-control" placeholder="Correo"/>
-                      </div>
-                  <div class="d-flex py-2">
-                      <div class="form-group col-6 col-md-6 p-1">
-                          <label for="">Cantidad</label>
-                          <input type="number" class="form-control" placeholder="Cantidad" />
-                      </div>
-                      <div class="form-group col-6 col-md-6 p-1">
-                          <label for="">Categoría</label>
-                          <select class="form-select" id="inputSelect">
-                              <option value="estudiante">Estudiante</option>
-                              <option value="junior">Junior</option>
-                              <option value="trainee">Trainee</option>
-                          </select>
-                      </div>
-                  </div>
-              </div>
-              <div class="form-row">
-                  <div class="form-group col-12 col-md-12 p-1 my-3">
-                      <div class="alert alert-primary" role="alert" id="alert">
-                          Total a Pagar: $
-                      </div>
-                  </div>
-              </div>
-              <div class="form-row d-flex gap-2 pb-1">
-                  <button type="reset" class="btn btn-lg btn-success btn btn-outline-light col-6" id="btn-borrar">Borrar</button>
-                  <button type="button" class="btn btn-lg btn-success btn btn-outline-light col-6" id="btn-resumen">Resumen</button>
-              </div>
-          </form>
-      </div>
-  </div>
-</div>
-`;
+    <div class="container">
+        <div class="row">
+            <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 py-3">
+                <div class="card-group gap-1">
+                    <div class="card">
+                        <div class="card-body border border-primary mr-1">
+                            <h5 class="card-title text-center">Estudiante</h5>
+                            <p class="card-text text-center">Tienen un descuento</p>
+                            <p class="card-title text-center fw-bolder">80%</p>
+                            <p class="card-text text-center">
+                                <small class="text-muted">
+                                * presentar documentación
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body border border-info mr-1 ">
+                            <h5 class="card-title text-center">Trainee</h5>
+                            <p class="card-text text-center">Tienen un descuento</p>
+                            <p class="card-title text-center fw-bolder">50%</p>
+                            <p class="card-text text-center">
+                                <small class="text-muted">
+                                * presentar documentación
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body border border-warning mr-1">
+                            <h5 class="card-title text-center">Junior</h5>
+                            <p class="card-text text-center">Tienen un descuento</p>
+                            <p class="card-title text-center fw-bolder">15%</p>
+                            <p class="card-text text-center">
+                                <small class="text-muted">
+                                * presentar documentación
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+        <div class="row">
+            <div class="col text-uppercase text-center">
+                <small>venta</small>
+                <h2>valor de ticket $200</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 py-1">
+                <form action="index.html" method="POST" class="w-100">
+                    <div class="form-row">
+                        <div class="d-flex py-2">
+                            <div class="form-group col-6 col-md-6 p-1">
+                                <input type="text" class="form-control" placeholder="Nombre" />
+                            </div>
+                            <div class="form-group col-6 col-md-6 p-1">
+                                <input type="text" class="form-control" placeholder="Apellido" />
+                            </div>
+                        </div>
+                            <div class="form-group col-12 col-md-12 p-1">
+                                <input type="email" class="form-control" placeholder="Correo"/>
+                            </div>
+                        <div class="d-flex py-2">
+                            <div class="form-group col-6 col-md-6 p-1">
+                                <label for="">Cantidad</label>
+                                <input type="number" class="form-control" placeholder="Cantidad" />
+                            </div>
+                            <div class="form-group col-6 col-md-6 p-1">
+                                <label for="">Categoría</label>
+                                <select class="form-select" id="inputSelect">
+                                    <option value="estudiante">Estudiante</option>
+                                    <option value="junior">Junior</option>
+                                    <option value="trainee">Trainee</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-12 col-md-12 p-1 my-3">
+                            <div class="alert alert-primary" role="alert" id="alert">
+                                Total a Pagar: $
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row d-flex gap-2 pb-1">
+                        <button type="reset" class="btn btn-lg btn-success btn btn-outline-light col-6" id="btn-borrar">Borrar</button>
+                        <button type="button" class="btn btn-lg btn-success btn btn-outline-light col-6" id="btn-resumen">Resumen</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    `;
   document
     .getElementById("btn-resumen")
     .addEventListener("click", mostrarTicket);
   document.getElementById("btn-borrar").addEventListener("click", borrarTicket);
 };
+
+//TODO: el type del botón Resumen es "button". En el form la action es index.html y el método es POST
 
 const calcularTicket = (perc1, perc2, perc3) => {
   const precioTicket = 200;
@@ -132,13 +134,13 @@ const mostrarTicket = () => {
 
   let alertTotalAPagar = document.getElementById("alert");
   alertTotalAPagar.innerHTML = `
-          Total a Pagar: $${total}
-      `;
+        Total a Pagar: $${total}
+    `;
 };
 
 const borrarTicket = () => {
   let alertTotalAPagar = document.getElementById("alert");
   alertTotalAPagar.innerHTML = `
-            Total a Pagar: $
-        `;
+          Total a Pagar: $
+      `;
 };
